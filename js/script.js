fc63eb91dcbdf5906f3bc4fda2d9fc2d03afea67
JHONDRASCODE <132631552+JHONDRASCODE@users.noreply.github.com>
2024-04-12 14:49:58 -0500

    finalizado

--- js/script.js
+++ js/script.js
@@ -1,73 +1,102 @@
-// Inicialización de variables
 let tarjetasDestapadas = 0;
-let tarjetasDestapadasAnteriores = 0;
 let tarjeta1 = null;
-let tarjeta2 = null;
+let tarjeta2 = null; 
 let primerResultado = null;
 let segundoResultado = null;
 let movimientos = 0;
 let aciertos = 0;
+let temporizador = false;
+let timer = 30;
+let timerInicial = 30;
+let tiempoRegresivoId = null;
 
-// Apuntado a documento HTML
-let mostrarMovimientos = document.getElementById("movimientos");
-let mostrarAciertos = document.getElementById("aciertos");
 
-//generacion de numeros aleatorios
-let numeros = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-numeros = numeros.sort(() => Math.random() - 0.5);
+//documento html
+let mostrarMovimientos = document.getElementById('movimientos');
+let mostarAciertos = document.getElementById('aciertos');
+let mostrarTiempo = document.getElementById('t-restante');
+
+//Generacion de numeros aleatorios
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+
+numeros = numeros.sort(()=>{return Math.random()-0.5});
 console.log(numeros);
 
-//Primera Funcion
-function destapar(id) {
-  tarjetasDestapadas++;
-  console.log(tarjetasDestapadas);
-  if (tarjetasDestapadas == 1) {
-    //Mostrar promer numero
-    tarjeta1 = document.getElementById(id);
-    primerResultado = numeros[id];
-    tarjeta1.innerHTML = primerResultado;
-
-    //desabilitar el primer boton
-    tarjeta1.disabled = true;
-  } else if (tarjetasDestapadas == 2) {
-    // mostrar segundo numero
-    tarjeta2 = document.getElementById(id);
-    segundoResultado = numeros[id];
-    tarjeta2.innerHTML = segundoResultado;
-
-    // Deshabilitr segundo boton
-    tarjeta2.disabled = true;
-
-    //incrementar movimientos
-    movimientos++;
-    mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
-
-    //comparar numeros y actualizar aciertos
-    if (tarjetasDestapadasAnteriores === 0) {
-      tarjetasDestapadasAnteriores = tarjetasDestapadas;
-    } else {
-      if (primerResultado === segundoResultado) {
-        // encerrra contador tarjetas destapadas
-        tarjetasDestapadas = 0;
-
-        //Aumentar aciertos
-        aciertos++;
-        mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
-
-        if (aciertos === 8) {
-          mostrarAciertos.innerHTML = `Aciertos: ${aciertos} p`;
+//funciones
+function contarTiempo(){
+    tiempoRegresivoId = setInterval(()=>{
+        timer--;
+        mostrarTiempo.innerHTML = `Tiempo: ${timer} segundos`;
+        if(timer == 0){
+            clearInterval(tiempoRegresivoId);
+            bloquearTarjetas();
         }
-      } else {
-        // mostrar momentanament valores y volver a tapar
-        setTimeout(() => {
-          tarjeta1.innerHTML = "";
-          tarjeta2.innerHTML = "";
-          tarjeta1.disabled = false;
-          tarjeta2.disabled = false;
-          tarjetasDestapadas = 0;
-          tarjetasDestapadasAnteriores = 0;
-        }, 2000);
-      }
+    },1000);
+}
+
+function bloquearTarjetas(){
+    for (let i = 0; i<=15; i++){
+        let tarjetaBloqueada = document.getElementById(i);
+        tarjetaBloqueada.innerHTML = numeros[i];
+        tarjetaBloqueada.disabled = true;
     }
-  }
+}
+
+//fuction principal
+function destapar(id){
+
+    if(temporizador == false ){
+        contarTiempo();
+        temporizador = true;
+    }
+
+    tarjetasDestapadas++;
+    console.log(tarjetasDestapadas);
+
+    if(tarjetasDestapadas == 1){
+        //mostrar primer numero
+        tarjeta1 = document.getElementById(id);
+        primerResultado = numeros[id];
+        tarjeta1.innerHTML = primerResultado;
+
+        //deshabilitar primer boton
+        tarjeta1.disabled = true;
+    } else if(tarjetasDestapadas == 2){
+        //mostrar segundo numero
+        tarjeta2 = document.getElementById(id);
+        segundoResultado = numeros[id];
+        tarjeta2.innerHTML = segundoResultado;
+
+        //deshabilitar segundo boton
+        tarjeta2.disabled = true;
+
+        //incrementar movimientos
+        movimientos++;
+        mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+
+        if(primerResultado == segundoResultado){
+            //encerrar contador de tarjetas destapadas
+            tarjetasDestapadas = 0;
+
+            //aumentar aciertos
+            aciertos++;
+            mostarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+
+            if(aciertos == 8){
+                clearInterval(tiempoRegresivoId);
+                mostarAciertos.innerHTML = `Aciertos: ${aciertos} 😱`
+                mostrarTiempo.innerHTML = `Congratulations! 👍 solo demoraste ${timerInicial - timer} segundos`
+                mostrarMovimientos.innerHTML = `Movimientos: ${movimientos} 🤌`
+            }
+        }else{
+            //Mostrar momentaneamente valores y volver a tapar
+            setTimeout(()=>{
+                tarjeta1.innerHTML = ' ';
+                tarjeta2.innerHTML = ' ';
+                tarjeta1.disabled = false;
+                tarjeta2.disabled = false;
+                tarjetasDestapadas = 0; 
+            },800);
+        }
+    }
 }
